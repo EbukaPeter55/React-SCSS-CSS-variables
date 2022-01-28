270ae837d457954e40f8cb3dc8502a5fb55cafdb
ebuka <peterdbrainy5@gmail.com>
2022-01-29 00:57:32 +0100

    Create a NavBar component, add the navlinks, import the component to the app.js file

---
+++ src/NavBar/NavBar.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './NavBar.styles.css';
+
+
+
+const NavBar = () => {
+
+    return (
+
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                    <li>
+                        angular
+                    </li>
+                    <li>
+                        UX design
+                    </li>
+                    <li>
+                        UX design
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
